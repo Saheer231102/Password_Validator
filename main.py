a214f41946fb93294dfa9122e8bb4f05c9aed8ad
K Dada Saheer <104244728+Saheer231102@users.noreply.github.com>
2025-01-02 19:33:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+import regex as re
+
+def printStrongNess(inn):
+    n = len(inn)
+    marks = 0
+
+    hasLower = False
+    hasUpper = False
+    hasDigit = False
+    specialChar = False
+    normalChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 "
+
+    for i in inn: 
+        if (re.search("([a-z])", i)):
+            hasLower = True
+            marks +=1
+        elif (re.search("([A-Z])", i)):
+            hasUpper = True
+            marks +=1
+        elif(re.search("([0-9])", i)):
+            hasDigit = True
+            marks +=1
+        elif (re.search("[@_!#$%^&*()<>?/|}{~:]", i)):
+            specialChar = True
+            marks +=2
+        else:
+            continue
+
+    # Strength of the password 
+
+    if(marks>=16 and n>=8 ):
+        print("Strong")
+    elif(marks<=16 and marks>=8):
+        print("Moderate")
+    elif (marks<16 and marks>=8 and n<8 and n>=6):
+        print("Moderate")
+    else:
+        print("Weak")
+
+def main():
+    inn = input("Enter your password: ")
+    printStrongNess(inn)
+
+main()
